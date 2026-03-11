268bd3e0c73448df874925bfa54f20d6828e6cb4
Pragathi P B <pragathibasutkar@gmail.com>
2026-03-11 21:27:22 +0530

    Connected Supabase database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from supabase import create_client
 
 app = FastAPI()
 
+SUPABASE_URL = "https://aayrdukncfnzdmcchcts.supabase.co"
+SUPABASE_KEY = "sb_publishable_lJQoxOT_4NY5WF7FsDvBwA_xk75RNRx"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 @app.get("/")
 def home():
-    return {"message": "Password Manager Backend Running"}
+    return {"message": "Backend connected to Supabase"}
+
+@app.get("/users")
+def get_users():
+    response = supabase.table("users").select("*").execute()
+    return response.data
